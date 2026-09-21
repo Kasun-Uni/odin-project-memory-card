1b39335ae940dad94330e5bcb95cf5dc4561881b
Kasun <kasun.kanakarathne@unicast.ne.jp>
2026-09-21 23:10:21 +0900

    docs: React + Webpack + Babelのセットアップ

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./template.html",
+    }),
+  ],
+  devServer: {
+    port: 8080,
+    watchFiles: ["./template.html"],
+  },
+};
