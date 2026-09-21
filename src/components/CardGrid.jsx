5aa6a941084ffd552f644bd9d3f9097e9e6e1085
Kasun <kasun.kanakarathne@unicast.ne.jp>
2026-09-21 23:50:39 +0900

    feat: カード一覧を表示するCardGridコンポーネントを追加

---
+++ src/components/CardGrid.jsx
@@ -0,0 +1,20 @@
+import Card from "./Card.jsx";
+import "../styles/CardGrid.css";
+
+function CardGrid({ cards, onCardClick }) {
+  return (
+    <div className="card-grid">
+      {cards.map((card) => (
+        <Card
+          key={card.id}
+          id={card.id}
+          name={card.name}
+          image={card.image}
+          onCardClick={onCardClick}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default CardGrid;
